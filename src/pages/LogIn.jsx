4b14858c0d57d36758a0bc06ca4c7a61cf18cc02
Sahu <smitaranee.sahu@capgemini.com>
2025-12-02 00:16:57 +0530

    login as guest button fix

--- src/pages/LogIn.jsx
+++ src/pages/LogIn.jsx
@@ -76,12 +76,14 @@ export default function LogIn() {
                   </label>
                   <input ref={password} type="password" id="password" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required/>
                 </div>
+                <div className=' flex justify-start'>
                 <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                   Log In
                 </button>
-                <button onClick={handleLoginAsGuest} type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                <button onClick={handleLoginAsGuest} type="submit" className="ml-3 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                   LogIn as guest
                 </button>
+                </div>
               </form>
             </div>
           )}
